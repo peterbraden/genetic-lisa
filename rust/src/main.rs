83a9ad84f3b7ebcada1eff2e3a398bde10ba59b9
Peter Braden <peterbraden@peterbraden.co.uk>
2017-08-16 17:07:37 +0200

    fix maths

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,10 +3,10 @@ extern crate rand;
 extern crate jpeg_decoder;
 extern crate serde;
 extern crate serde_json;
-#[macro_use]
-extern crate lazy_static;
-#[macro_use]
-extern crate serde_derive;
+extern crate env_logger;
+#[macro_use] extern crate lazy_static;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate clap;
 
 use jpeg_decoder::Decoder;
 use std::fs::File;
@@ -14,15 +14,18 @@ use std::io::BufReader;
 use std::fmt::Write;
 use darwin_rs::{Individual, SimulationBuilder, PopulationBuilder};
 use rand::Rng;
+use clap::{Arg, App};
 
-const PIXELDEPTH:i32 = 3;
+const PIXELDEPTH:usize = 3;
+const WHITE:u32 = (256<<16) + (256<<8) + 256;
+
 
 lazy_static! {
 	static ref LISA: Context = Context::new("lisa.jpg");
 }
 
-fn coord(x: i32, y: i32) -> i32 {
-	return (x * LISA.width as i32 + y) * PIXELDEPTH;
+fn coord(x: i32, y: i32) -> usize {
+	return ((x * LISA.width as i32 + y) * PIXELDEPTH as i32) as usize;
 }
 
 fn rand() -> f64 {
@@ -38,7 +41,7 @@ fn from_rgb(r:u32, g:u32, b: u32) -> u32 {
 }
 
 fn color_add(c:u32, c2: u32, opacity: f64) -> u32 {
-	return c + (c2 as f64 * opacity) as u32;
+	return c.saturating_add((c2 as f64 * opacity) as u32) % WHITE;
 }
 
 fn r(c:u32) -> u8 {
@@ -53,8 +56,9 @@ fn b(c:u32) -> u8 {
 
 struct Context {
 	image: Vec<u8>,
-	width: u16,
-	height: u16
+	width: usize,
+	height: usize,
+    format: jpeg_decoder::PixelFormat
 }
 
 impl Context {
@@ -67,8 +71,9 @@ impl Context {
 
 		return Context {
 			image: image,
-			height: meta.height,
-			width: meta.width
+			height: meta.height as usize,
+			width: meta.width as usize,
+            format: meta.pixel_format
 		};
 	}
 }
@@ -131,16 +136,19 @@ impl Canvas {
 	}
 
 	pub fn draw(&mut self, c: &Circle) {
-		let xmin = - c.rad * LISA.width as f64;
-		let xmax = c.rad * LISA.width as f64;
-		let radrad = (c.rad * LISA.width as f64) * (c.rad * LISA.width as f64);
+        let rad = c.rad * LISA.width as f64;
+        let cx = c.x * LISA.width as f64;
+        let cy = c.x * LISA.height as f64;
+		let radrad = rad * rad;
 
-		for x in (xmin as i32)..(xmax as i32) {
+		for x in (- rad as i32)..(rad as i32) {
 			let height = (radrad - (x as f64 * x as f64)).sqrt();
 			for y in (-height as i32)..(height as i32) {
-				let i = coord(x + (c.x * LISA.width as f64) as i32, y + (c.y * LISA.height as f64) as i32) as usize;
+				let i = coord(x + cx as i32, y + cy as i32);
 
-				if x as f64 + c.x > 0. && y as f64 + c.y > 0. && i < (LISA.width * LISA.height) as usize {
+				if x as f64 + cx > 0. &&
+                   y as f64 + cy > 0. &&
+                   i < (LISA.width * LISA.height * PIXELDEPTH) as usize {
 					let color = from_rgb(self.pixels[i] as u32, self.pixels[i+1] as u32, self.pixels[i+2] as u32);
 					let newcolor = color_add(color, c.color, c.opacity);
 					self.pixels[i] = r(newcolor);
@@ -152,14 +160,15 @@ impl Canvas {
 	}
 
 	pub fn diff(&self) -> f64 {
-		let mut total = 0;
+		let mut total = 0.;
 		for x in 0..LISA.image.len() {
 			let pixdiff = LISA.image[x] as i32 - self.pixels[x] as i32;
-			total += pixdiff * pixdiff;
+			total += (pixdiff * pixdiff) as f64;
 		}
-		return total as f64;
+		return total;
 	}
 
+    /*
     pub fn to_pixels(&self) -> Vec<u32> {
         let mut out: Vec<u32> = Vec::new();
         for x in 0..LISA.width {
@@ -173,6 +182,7 @@ impl Canvas {
         }
         return out;
     }
+    */
 }
 
 
@@ -191,11 +201,10 @@ impl Lisa {
 		}
 	}
     pub fn create_with(data: Vec<Circle>) -> Lisa {
-        let mut res = Lisa {
+        let res = Lisa {
             circles: data,
             canv: Canvas::new()
         };
-        res.draw();
         return res;
     }
 
@@ -223,6 +232,7 @@ impl Lisa {
 
 impl Individual for Lisa {
     fn mutate(&mut self) {
+        // Add circle
 		if rand() < 0.2 {
 			self.circles.push(Circle::random());
 		}
@@ -232,6 +242,7 @@ impl Individual for Lisa {
             self.circles.retain(|_| rand() > 0.1 );
 		}
 
+        // Mutate random circle
         if rand() < 0.8 {
             match rand::thread_rng().choose_mut(&mut self.circles) {
                 Some(m) => {
@@ -255,13 +266,13 @@ impl Individual for Lisa {
             }
         }
 
-		self.draw();
+        // Merge circles?
     }
 
     fn calculate_fitness(&mut self) -> f64 {
+		self.draw();
 		let fitness = self.canv.diff() * (1. + 0.0001 * (self.circles.len() as f64));
         // Pixel difference * 100% + 0.01% per circle
-		//print!(". {}\n", fitness);
 		return fitness;
     }
 
@@ -271,7 +282,7 @@ impl Individual for Lisa {
     }
 
 	fn new_fittest_found(&mut self) {
-		print!("\nNew fittest: {:?} {}", self.calculate_fitness(), self.circles.len());
+		print!("\nNew fittest: {:.1} {}", self.calculate_fitness(), self.circles.len());
         let mut svg = File::create("best.svg").unwrap();
         std::io::Write::write_all(&mut svg, self.svg().as_bytes()).expect("couldn't write");
 
@@ -282,7 +293,7 @@ impl Individual for Lisa {
     }
 }
 
-fn make_population(count: u32) -> Vec<Lisa> {
+fn make_population(count: usize) -> Vec<Lisa> {
 	let mut result = Vec::new();
 
     for _ in 0..count {
@@ -293,7 +304,7 @@ fn make_population(count: u32) -> Vec<Lisa> {
 	return result;
 }
 
-fn make_population_from_file(count: u32, path: &str) -> Vec<Lisa> {
+fn make_population_from_file(count: usize, path: &str) -> Vec<Lisa> {
 	let mut result = Vec::new();
     let f = File::open(path).unwrap();
     let saved: Vec<Circle> = serde_json::from_reader(f).unwrap();
@@ -307,25 +318,47 @@ fn make_population_from_file(count: u32, path: &str) -> Vec<Lisa> {
 }
 
 fn main() {
-	println!("Loaded source image {}x{}", LISA.width, LISA.height);
-	//let my_pop = make_population(100);
-	let my_pop = make_population_from_file(10, "best.json");
-	println!("Allocated individuals");
+    env_logger::init();
+    let matches = App::new("Lisa")
+                 .arg(Arg::with_name("population")
+                    .short("p")
+                    .takes_value(true))
+                 .arg(Arg::with_name("loadbest")
+                    .short("b"))
+                 .arg(Arg::with_name("exponential-growth")
+                    .short("e")
+                    .takes_value(true))
+                 .get_matches();
+
+    let population = value_t!(matches.value_of("population"), usize).unwrap_or(10);
+    let growth = value_t!(matches.value_of("exponential-growth"), f64).unwrap_or(1.0);
+    let start_with_best = matches.is_present("loadbest"); 
+    let mut my_pop;
+
+	println!("# Loaded source image {}x{} {:?}", LISA.width, LISA.height, LISA.format);
+    if start_with_best {
+        my_pop = make_population_from_file(population, "best.json");
+        println!("# - Using previous best: {:.1}, {}",
+                 my_pop[0].calculate_fitness(), my_pop[0].circles.len());
+    } else {
+	    my_pop = make_population(population);
+    }
+	println!("# Allocated individuals");
 	let population = PopulationBuilder::<Lisa>::new()
 		.set_id(1)
 		.initial_population(&my_pop)
-		.increasing_exp_mutation_rate(1.05)
+		.increasing_exp_mutation_rate(growth)
 		.reset_limit_increment(100)
 		.reset_limit_start(100)
 		.reset_limit_end(0)
 		.finalize().unwrap();
-	println!("Built population");
+	println!("# Built population");
 	let simulation = SimulationBuilder::<Lisa>::new()
 		.fitness(0.0)
         .threads(2)
         .add_population(population)
 		.finalize();
-	println!("Initialized simulation");
+	println!("# Initialized simulation");
 
 
 	match simulation {
